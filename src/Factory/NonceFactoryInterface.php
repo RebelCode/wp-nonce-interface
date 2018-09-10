9ed03b43b7e2f60ea266a3c653dcdf4f91c496ac
Miguel Muscat <miguelmuscat93@gmail.com>
2018-09-10 11:27:39 +0200

    Fixed factory interface name

---
+++ src/Factory/NonceFactoryInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace RebelCode\WordPress\Nonce\Factory;
+
+use RebelCode\WordPress\Nonce\NonceInterface;
+
+/**
+ * Something that can create nonce instances.
+ *
+ * @since [*next-version*]
+ */
+interface NonceFactoryInterface
+{
+    /**
+     * The key of the ID in factory config data.
+     *
+     * @since [*next-version*]
+     */
+    const K_CONFIG_ID = 'id';
+
+    /**
+     * The key of the code in factory config data.
+     *
+     * @since [*next-version*]
+     */
+    const K_CONFIG_CODE = 'code';
+
+    /**
+     * Creates a new nonce instance.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $config Configuration data.
+     *
+     * @return NonceInterface The created nonce instance.
+     */
+    public function make($config = []);
+}
